7f8d5df2999e885984f8893a72212126c0d7a6d3
Ujual <ujualas4@gmail.com>
2023-07-29 14:48:50 +0530

    navclr change

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
         $('#collapsibleNavbar').removeClass("bg-dark").addClass("navclr")
         $('.img-thumbnail').removeClass('img1').addClass("img2")
         $('.n1').removeClass('name').addClass('n2')
+        $('.navbar').removeClass('navbar-dark bg-dark').addClass('navbar-light navclr')
     });
     $('.dark').click(function() {
         $('body').removeClass("bglight").addClass('bg')
         $('#collapsibleNavbar').addClass("bg-dark").removeClass("navclr")
         $('.n1').addClass('name').removeClass('n2')
         $('.img-thumbnail').removeClass('img2').addClass("img1")
+        $('.navbar').addClass('navbar-dark bg-dark').removeClass('bg-light navbar-light')
     });
 });
