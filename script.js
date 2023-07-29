ec443b8fa12db5f4b2476284b7b5822702528916
Ujual <ujualas4@gmail.com>
2023-07-29 14:30:11 +0530

    part1

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.light').click(function() {
+        $('body').removeClass('bg').addClass("bglight")
+        $('#collapsibleNavbar').removeClass("bg-dark").addClass("navclr")
+        $('.img-thumbnail').removeClass('img1').addClass("img2")
+        $('.n1').removeClass('name').addClass('n2')
+    });
+    $('.dark').click(function() {
+        $('body').removeClass("bglight").addClass('bg')
+        $('#collapsibleNavbar').addClass("bg-dark").removeClass("navclr")
+        $('.n1').addClass('name').removeClass('n2')
+        $('.img-thumbnail').removeClass('img2').addClass("img1")
+    });
+});
