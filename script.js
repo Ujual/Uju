c840e61e1d17d77fde43cad8db8766cd1e191fcb
Ujual <ujualas4@gmail.com>
2023-08-01 11:46:42 +0530

    changes about

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ $(document).ready(function() {
     $('.light').click(function() {
         $('body').removeClass('bg').addClass("bglight")
         $('#collapsibleNavbar').removeClass("bg-dark").addClass("navclr")
-        $('.img-thumbnail').removeClass('img1').addClass("img2")
+        $('.imm').removeClass('img1').addClass("img2")
         $('.n1').removeClass('name').addClass('n2')
         $('.navbar').removeClass('navbar-dark bg-dark').addClass('navbar-light navclr')
+        $('.abt').removeClass('bg-dark').addClass('bg-light')
+        $('.imm1').removeClass('imgabt').addClass("imgabt1")
     });
     $('.dark').click(function() {
         $('body').removeClass("bglight").addClass('bg')
         $('#collapsibleNavbar').addClass("bg-dark").removeClass("navclr")
         $('.n1').addClass('name').removeClass('n2')
-        $('.img-thumbnail').removeClass('img2').addClass("img1")
+        $('.imm').removeClass('img2').addClass("img1")
         $('.navbar').addClass('navbar-dark bg-dark').removeClass('bg-light navbar-light')
+        $('.abt').addClass('bg-dark').removeClass('bg-light')
+        $('.imm1').addClass('imgabt').removeClass("imgabt1")
+
     });
     $('.pt-trigger').click(function() {
         $('#home').fadeIn(1000);
@@ -48,7 +53,5 @@ $(document).ready(function() {
         $('#portfolio').hide("slow");
         $('#contact').fadeIn(1000);
         $('.collapsibleNavbar').fadeOut("slow");
-
-
     });
 });
